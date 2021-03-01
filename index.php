18ef694ba78460c6456cbcb70ea7114d43342543
davide <davidecorsinimilano@gmail.com>
2021-03-01 16:25:08 +0100

    layout

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
     // foreach($database as $key => $value){
     //     echo $value['genre'];
     // }
+
+
 ?>
 
 <!DOCTYPE html>
@@ -16,14 +18,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tidal ascolta la tua musica quando vuoi dove vuoi creato da parte di Jay-z with Bayonce</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"> 
+
 </head>
 <body>
     
     <!-- start header -->
     <header>
         <div class="container">
-            <div class="nav-dx">
-                <ul class="menu-dx">
+            <div class="nav-sx">
+                <ul class="menu-sx">
                     <li><a href="#">
                         <img src="img/logo-tidal.jpg" alt="">
                     </a></li>
@@ -33,10 +38,10 @@
                     <li><a href="#">A proposito di TIDAL</a></li>
                 </ul>
             </div>
-            <div class="nav-sx">
-                <ul>
-                    <li><a href="#">prova</a></li>
-                    <li><a href="#">prova</a></li>
+            <div class="nav-dx">
+                <ul class="menu-dx">
+                    <li><a href="#">Login</a></li>
+                    <li><a href="#" class="btn btn-classic">prova</a></li>
                 </ul>
             </div>
         </div>
@@ -45,7 +50,26 @@
 
     <!-- start main -->
     <main>
+        <div class="container">
+            <h2 style="color: white;">NAVIGA</h2>
 
+            <div class="box-main" style="color: white;">
+                <?php foreach($database as $key => $value) {?>
+                    <div class="post">
+                        <h3>
+                            <?php echo $value['author']; ?>
+                        </h3>
+                        <img src="<?php echo $value['poster']; ?>" alt="">
+                        <h4>
+                            <?php echo $value['genre']; ?>
+                        </h4>
+                        <p>
+                            <?php echo $value['year']; ?>
+                        </p>
+                    </div>
+                    <?php } ?>
+            </div>
+        </div>
     </main>
     <!-- /main -->
 
