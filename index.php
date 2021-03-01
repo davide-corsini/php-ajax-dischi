c079b0e1657bc6e3b4f574048848d6ce382cfdfe
davide <davidecorsinimilano@gmail.com>
2021-03-01 15:47:24 +0100

    w  on

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php 
+    include 'db.php';
+
+
+
+    // foreach($database as $key => $value){
+    //     echo $value['genre'];
+    // }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tidal ascolta la tua musica quando vuoi dove vuoi creato da parte di Jay-z with Bayonce</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    
+    <!-- start header -->
+    <header>
+        <div class="container">
+            <div class="nav-dx">
+                <ul class="menu-dx">
+                    <li><a href="#">
+                        <img src="img/logo-tidal.jpg" alt="">
+                    </a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">In primo piano</a></li>
+                    <li><a href="#">Magazine</a></li>
+                    <li><a href="#">A proposito di TIDAL</a></li>
+                </ul>
+            </div>
+            <div class="nav-sx">
+                <ul>
+                    <li><a href="#">prova</a></li>
+                    <li><a href="#">prova</a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
+    <!-- /header -->
+
+    <!-- start main -->
+    <main>
+
+    </main>
+    <!-- /main -->
+
+    <!-- start foot -->
+    <footer>
+
+    </footer>
+    <!-- /foot -->
+</body>
+</html>
