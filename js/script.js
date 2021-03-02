079e37409c919d89737690a4ce738e20f8054ab8
davide <davidecorsinimilano@gmail.com>
2021-03-02 19:52:56 +0100

    layout vue

--- js/script.js
+++ js/script.js
@@ -7,7 +7,69 @@
   \***********************/
 /***/ (() => {
 
+var app = new Vue({
+  el: '#app',
+  data: {
+    arrayDischi: '',
+    generi: ['All'],
+    search: 'All',
+    contatore: 0,
+    arrayEffects: [{
+      logo: "https://tidal.com/_nuxt/img/5.d4dd1c0.jpg",
+      title: 'Oltre 70 milioni di canzoni. Senza pubblicitá.',
+      descrizione: 'Con oltre 70 milioni di brani, uscite esclusive e tonnellate di interviste e video musicali, TIDAL ti avvicina agli artisti che ascolti. Completamente senza pubblicitá.'
+    }, {
+      logo: "https://tidal.com/_nuxt/img/phone.e0723a9.png",
+      title: 'Mix e radio.',
+      descrizione: 'Ottieni una selezione personalizzata di brani e video con My Mix o scopri nuova musica con Artist Radio.'
+    }, {
+      logo: "https://www.tpi.it/app/uploads/2018/03/Spotify.jpg",
+      title: 'Playlist selezionate con cura.',
+      descrizione: "Dai un'occhiata alle playlist originali selezionate personalmente dal nostro team di esperti o ascolta in streaming le playlist degli ospiti curate dagli artisti che ami."
+    }, {
+      logo: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone11-select-2019-family_GEO_EMEA?wid=882&hei=1058&fmt=jpeg&qlt=80&op_usm=0.5,0.5&.v=1567022219953",
+      title: 'Video e livestream.',
+      descrizione: "Guarda oltre 250.000 video di alta qualitá, tra cui video musicali, contenuti originali, livestream esclusivi ed eventi."
+    }, {
+      logo: "https://www.droid-life.com/wp-content/uploads/2016/02/tidal-jay-z-samsung-980x653.jpg",
+      title: 'In qualsiasi momento. Dovunque.',
+      descrizione: "Ascolta quando vuoi e su qualsiasi dispositivo, che tu sia offline, in mezzo alla natura, a casa o in macchina."
+    }]
+  },
+  mounted: function mounted() {
+    var _this = this;
 
+    axios.get('server.php').then(function (result) {
+      _this.arrayDischi = result.data;
+      console.log(_this.arrayDischi);
+      _this.arrayDischiFiltrato = _this.arrayDischi;
+
+      _this.arrayDischi.forEach(function (element) {
+        if (!_this.generi.includes(element.genre)) {
+          _this.generi.push(element.genre);
+        }
+
+        console.log(_this.generi);
+      });
+    });
+  },
+  methods: {
+    filterBy: function filterBy() {
+      var _this2 = this;
+
+      if (this.search == "All") {
+        this.arrayDischiFiltrato = this.arrayDischi;
+      } else {
+        this.arrayDischiFiltrato = this.arrayDischi.filter(function (element) {
+          return element.genre.match(_this2.search);
+        });
+      }
+    },
+    choiceEffect: function choiceEffect(indexFunction) {
+      this.contatore = indexFunction;
+    }
+  }
+});
 
 /***/ }),
 
--- src/script.js
+++ src/script.js
@@ -3,7 +3,35 @@ var app = new Vue({
     data:{
         arrayDischi: '',
         generi: ['All'],
-        search: 'All'
+        search: 'All',
+        contatore: 0,
+        arrayEffects : [
+            {
+                logo: "https://tidal.com/_nuxt/img/5.d4dd1c0.jpg",
+                title: 'Oltre 70 milioni di canzoni. Senza pubblicitá.',
+                descrizione: 'Con oltre 70 milioni di brani, uscite esclusive e tonnellate di interviste e video musicali, TIDAL ti avvicina agli artisti che ascolti. Completamente senza pubblicitá.'
+            },
+            {
+                logo: "https://tidal.com/_nuxt/img/phone.e0723a9.png",
+                title: 'Mix e radio.',
+                descrizione: 'Ottieni una selezione personalizzata di brani e video con My Mix o scopri nuova musica con Artist Radio.'
+            },
+            {
+                logo: "https://www.tpi.it/app/uploads/2018/03/Spotify.jpg",
+                title: 'Playlist selezionate con cura.',
+                descrizione: "Dai un'occhiata alle playlist originali selezionate personalmente dal nostro team di esperti o ascolta in streaming le playlist degli ospiti curate dagli artisti che ami."
+            },
+            {
+                logo: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone11-select-2019-family_GEO_EMEA?wid=882&hei=1058&fmt=jpeg&qlt=80&op_usm=0.5,0.5&.v=1567022219953",
+                title: 'Video e livestream.',
+                descrizione: "Guarda oltre 250.000 video di alta qualitá, tra cui video musicali, contenuti originali, livestream esclusivi ed eventi."
+            },
+            {
+                logo: "https://www.droid-life.com/wp-content/uploads/2016/02/tidal-jay-z-samsung-980x653.jpg",
+                title: 'In qualsiasi momento. Dovunque.',
+                descrizione: "Ascolta quando vuoi e su qualsiasi dispositivo, che tu sia offline, in mezzo alla natura, a casa o in macchina."
+            }
+        ]
     },
     mounted(){
         axios
@@ -33,6 +61,9 @@ var app = new Vue({
                     
                 })
             }
+        },
+        choiceEffect(indexFunction){
+            this.contatore = indexFunction;
         }
     }
 })
